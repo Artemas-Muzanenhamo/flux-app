35ca59d3ef045b4b59f1100a03f697381741c7b3
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-09-01 03:03:25 +0100

    polish up

--- movie-service/src/main/java/com/movies/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/movies/movieservice/MovieServiceApplication.java
@@ -16,22 +16,26 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class MovieServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieServiceApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner demo(MovieRepository movieRepository) {
-		return args -> {
-			Stream.of("Aeon Flux", "Enter the Mono<Void>", "The Fluxinator", "Silence of the Lambdas",
-					"Back to the future", "Y Tu Mono Tambien")
-					.map(name -> new Movie(UUID.randomUUID().toString(), name, randomGenre()))
-					.forEach(movie -> movieRepository.save(movie).subscribe(System.out::println));
-		};
-	}
+    @Bean
+    CommandLineRunner demo(MovieRepository movieRepository) {
+        return args -> {
 
-	private String randomGenre() {
-		String[] genres = "horror, drama, documentary, action, sci-fi".split(",");
-		return genres[new Random().nextInt(genres.length)];
-	}
+            movieRepository.deleteAll()
+                    .subscribe(null, null, () -> Stream.of("Aeon Flux", "Enter the Mono<Void>",
+                            "The Fluxinator", "Silence of the Lambdas", "Back to the future", "Y Tu Mono Tambien",
+                            "Reactive Mongos on a Plane")
+                            .map(name -> new Movie(UUID.randomUUID().toString(), name, randomGenre()))
+                            .forEach(movie -> movieRepository.save(movie).subscribe(System.out::println))
+                    );
+        };
+    }
+
+    private String randomGenre() {
+        String[] genres = "horror, drama, documentary, action, sci-fi".split(",");
+        return genres[new Random().nextInt(genres.length)];
+    }
 }
