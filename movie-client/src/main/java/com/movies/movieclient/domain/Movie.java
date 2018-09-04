43750dc6632cabacf85336606a2a19378cc3ec7d
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-09-04 14:17:53 +0100

    add WebClient

--- movie-client/src/main/java/com/movies/movieclient/domain/Movie.java
+++ movie-client/src/main/java/com/movies/movieclient/domain/Movie.java
@@ -3,9 +3,41 @@ package com.movies.movieclient.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class Movie {
     private String id;
     private String title;
+
+    public Movie() {
+    }
+
+    public Movie(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (id != null ? !id.equals(movie.id) : movie.id != null) return false;
+        return title != null ? title.equals(movie.title) : movie.title == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
 }
