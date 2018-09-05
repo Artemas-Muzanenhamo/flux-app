704836a3236a2f00c36037a778dc4fe8a59fabfc
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-09-05 08:43:48 +0100

    add Movie Client runner

--- movie-client/src/main/java/com/movies/movieclient/MovieClientApplication.java
+++ movie-client/src/main/java/com/movies/movieclient/MovieClientApplication.java
@@ -2,12 +2,14 @@ package com.movies.movieclient;
 
 import com.movies.movieclient.domain.Movie;
 import com.movies.movieclient.domain.MovieEvent;
+import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Log
 @SpringBootApplication
 public class MovieClientApplication {
 
@@ -18,20 +20,17 @@ public class MovieClientApplication {
 
     @Bean
     CommandLineRunner demo(WebClient webClient) {
-        return args -> {
-            webClient.get()
-                    .uri("http://localhost:8080/movies")
-                    .exchange()
-                    .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Movie.class))
-                    .filter(movie -> movie.getTitle().toLowerCase().contains("Aeon Flux".toLowerCase()))
-                    .subscribe(movie ->
-                            webClient.get()
-                                    .uri("http://localhost:8080/movies/{id}/events", movie.getId())
-                                    .exchange()
-                                    .flatMapMany(clientResponse -> clientResponse.bodyToFlux(MovieEvent.class))
-                                    .subscribe(System.out::println));
-
-        };
+        return args -> webClient.get()
+                .uri("http://localhost:8082/movies")
+                .exchange()
+                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Movie.class))
+                .filter(movie -> movie.getTitle().toLowerCase().contains("Aeon Flux".toLowerCase()))
+                .subscribe(movie ->
+                        webClient.get()
+                                .uri("http://localhost:8082/movies/{id}/events", movie.getId())
+                                .exchange()
+                                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(MovieEvent.class))
+                                .subscribe(movieEvent -> log.info(movieEvent.toString())));
     }
 
     public static void main(String[] args) {
