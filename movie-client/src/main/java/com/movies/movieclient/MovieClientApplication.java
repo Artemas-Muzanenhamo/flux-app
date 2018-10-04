e6a611544422db6d0cc57f4f1481df099b7b5ec5
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-10-04 18:45:19 +0100

    amend MovieClient

--- movie-client/src/main/java/com/movies/movieclient/MovieClientApplication.java
+++ movie-client/src/main/java/com/movies/movieclient/MovieClientApplication.java
@@ -3,15 +3,18 @@ package com.movies.movieclient;
 import com.movies.movieclient.domain.Movie;
 import com.movies.movieclient.domain.MovieEvent;
 import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Log
 @SpringBootApplication
 public class MovieClientApplication {
+    Logger logger = LoggerFactory.getLogger(MovieClientApplication.class);
 
     @Bean
     WebClient webClient() {
@@ -22,16 +25,19 @@ public class MovieClientApplication {
     CommandLineRunner demo(WebClient webClient) {
         return args -> webClient.get()
                 .uri("http://localhost:8082/movies")
-                .accept(MediaType.TEXT_EVENT_STREAM)
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Movie.class))
                 .filter(movie -> movie.getTitle().toLowerCase().contains("Aeon Flux".toLowerCase()))
                 .subscribe(movie ->
                         webClient.get()
                                 .uri("http://localhost:8082/movies/{id}/events", movie.getId())
+                                .accept(MediaType.TEXT_EVENT_STREAM)
                                 .exchange()
                                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(MovieEvent.class))
-                                .subscribe(movieEvent -> log.info(movieEvent.toString())));
+                                .subscribe(movieEvent -> {
+                                    System.out.println(movieEvent.toString());
+                                    logger.info(movieEvent.toString());
+                                }));
     }
 
     public static void main(String[] args) {
